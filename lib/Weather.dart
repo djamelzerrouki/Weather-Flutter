e5d7a086cd406a95a168fc10b854d31e572c284a
zerrouki djamel <djameljimmizerrouki@gmail.com>
2020-05-15 22:42:34 +0200

    🎈"

--- lib/Weather.dart
+++ lib/Weather.dart
@@ -24,34 +24,72 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Wheathr of city ${widget.city}'),
-        backgroundColor: Colors.teal,
-      ),
+    return  Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/imagegif.gif"), fit: BoxFit.cover)),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              title: Text('Wheathr of city ${widget.city}',
+                style: TextStyle(color:Colors.black),
+              ),
+               centerTitle: true,
+
+            ),
+
+
+
       body: (weatherDate==null?
       Center(child: CircularProgressIndicator(),):
           ListView.builder(itemCount: (weatherDate==null?0:weatherDate['list'].length),
           itemBuilder: (context,index){
             return Card(
-              color: Colors.teal,
-              child: Row(
+              color: Colors.black45,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
 
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  CircleAvatar(
-                    //images/
-                    backgroundImage: AssetImage(
-                        "assets/images/${weatherDate['list'][index]['weather'][0]['main'].toString().toLowerCase()}.png"),
-                  ),
-                  Text("${new DateFormat('E-dd/MM/yyyy').format(DateTime.fromMicrosecondsSinceEpoch(weatherDate['list'][index]['dt']*1000000))}" ),
-                  Text("${(weatherDate['list'][index]['main']['temp']).round()} °C"),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                       children: <Widget>[
+                        CircleAvatar(
+                          //images/
+                          backgroundImage: AssetImage(
+                              "assets/images/${weatherDate['list'][index]['weather'][0]['main'].toString().toLowerCase()}.png"),
+                        ),
 
-                ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("${new DateFormat('E-dd/MM/yyyy').format(DateTime.fromMicrosecondsSinceEpoch(weatherDate['list'][index]['dt']*1000000))}",
+                              style: TextStyle(color:Colors.white, fontSize: 18,fontWeight: FontWeight.bold),
+                              ),
+                              Text("${new DateFormat('HH:mm').format(DateTime.fromMicrosecondsSinceEpoch(weatherDate['list'][index]['dt']*1000000))} "
+                                  " | ${weatherDate['list'][index]['weather'][0]['main'].toString().toLowerCase()}",
+                                style: TextStyle(color:Colors.amber, fontSize: 18,fontWeight: FontWeight.bold),
+                              ),
+
+                            ],
+
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text("${(weatherDate['list'][index]['main']['temp']).round()} °C",
+                      style: TextStyle(color:Colors.lightGreen, fontSize: 22,fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
             );
           })
-      ),
+      ),),
     );
   }
 
